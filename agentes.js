966dd87c9559c146eeb2833b4ef08378b3f1acfd
Cheriefox <164426395+Cheriefox@users.noreply.github.com>
2024-11-22 17:10:39 -0300

    Update agentes.js

--- agentes.js
+++ agentes.js
@@ -1,25 +1,24 @@
 async function cargarAgentes() {
-    try{
-        const response = await fetch('https://valorant-api.com/v1/agents'); 
-        const data = await response.json(); 
+    try {
+        const response = await fetch('https://valorant-api.com/v1/agents');
+        const data = await response.json();
 
         const agentes = data.data;
         console.log('Agentes:', agentes);
-        
-    
+
         const listaAgentes = document.querySelector('ul');
 
         agentes.forEach(agente => {
-        const li = document.createElement('li');
-        li.className = 'agentes col-2 text-center'; 
-        li.innerHTML = `
-            <img src="${agente.displayIcon}" width="50%" alt="Ícono de ${agente.displayName}" class="img-fluid">
-            <p>${agente.displayName}</p>
-        `;
-        listaAgentes.appendChild(li); 
-    })
+            const li = document.createElement('li');
+            li.className = 'agentes col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 text-center';
+            li.innerHTML = `
+                <img src="${agente.displayIcon}" width="50%" alt="Ícono de ${agente.displayName}" class="img-fluid">
+                <p>${agente.displayName}</p>
+            `;
+            listaAgentes.appendChild(li);
+        });
     } catch (error) {
         console.error('Error al cargar el agente:', error);
     }
-    }  
-cargarAgentes(); 
+}
+cargarAgentes();
