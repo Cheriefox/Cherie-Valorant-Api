5f4c2f2542d39fa1620bd4e736b7c6cfdb34b89d
Cheriefox <164426395+Cheriefox@users.noreply.github.com>
2024-11-25 16:17:52 -0300

    Update agentes.js

--- agentes.js
+++ agentes.js
@@ -2,20 +2,25 @@ async function cargarAgentes() {
     try {
         const response = await fetch('https://valorant-api.com/v1/agents');
         const data = await response.json();
-
         const agentes = data.data;
-        console.log('Datos completos de la API:', agentes);  // Imprimimos todos los agentes para verificar la estructura
+        console.log('Datos completos de la API:', agentes);
 
-        // Filtramos el agente con el UUID específico
-        const agentesFiltrados = agentes.filter(agente => agente.uuid !== 'ded3520f-4264-bfed-162d-b080e2abccf9'); // Excluimos el agente con ese UUID
+        const agentesFiltrados = agentes.filter(agente => agente.uuid !== 'ded3520f-4264-bfed-162d-b080e2abccf9');
 
         const listaAgentes = document.querySelector('ul');
 
         agentesFiltrados.forEach(agente => {
             const li = document.createElement('li');
             li.className = 'agentes col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 text-center';
+            
+            // Al hacer clic en el agente, guardar el uuid en el localStorage y redirigir
+            li.addEventListener('click', () => {
+                localStorage.setItem('selectedAgentUuid', agente.uuid);
+                window.location.href = 'descripcion.html';  // Redirigir a la página de descripción
+            });
+
             li.innerHTML = `
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                <a href="descripcion.html">
                 <img src="${agente.displayIcon}" width="50%" alt="Ícono de ${agente.displayName}" class="img-fluid"></a>
                 <p>${agente.displayName}</p>
             `;
